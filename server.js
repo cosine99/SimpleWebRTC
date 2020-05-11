45d1dd2ada49c0ddbd8d469b53ac3f79a8c43ac0
AJ <hrishikesh2410@gmail.com>
2020-05-12 00:03:40 +0530

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
+var port = 3000 || proces.env.PORT;
 
 // Set public folder as root
 app.use(express.static('public'));
